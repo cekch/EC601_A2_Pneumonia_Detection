a1ab8abedb6ebc4385daedc9ca74f5f722b15402
Caroline <cekchian@bu.edu>
2018-12-07 18:00:12 -0500

    Added functionality for displaying plots in web app of the x rays and the bounding box around the lung opacity. Currently just plotting a single generic bounding box.

--- Web_Application/main.py
+++ Web_Application/main.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename  
+from werkzeug import secure_filename
+import pydicom
+import numpy as np
+from PIL import Image
+import pylab
+import matplotlib.pyplot as plt
+#import base64
+#import plot_bounding_boxes
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,7 +26,44 @@ def upload_file2():
 	if request.method == 'POST':
 		f = request.files['file']
 		f.save(secure_filename(f.filename))
-		return 'file uploaded successfully'
+		d = pydicom.read_file(f.filename)
+		im = d.pixel_array
+		im = np.stack([im] * 3, axis=2)
+		x = 640
+		y = 450
+		width = 65
+		height = 188
+		display_bbox(x, y, width, height, im)
+		#return 'file uploaded successfully'
+        return render_template('patient_plot.html', name = 'patient plot', url = '/static/images/patient_plot.png')
+
+def overlay_box(im, box, rgb, stroke=1):
+    """
+    Method to overlay single box on image
+    """
+    # --- Convert coordinates to integers
+    box = [int(float(b)) for b in box]
+
+    # --- Extract coordinates
+    y1, x1, height, width = box
+    y2 = y1 + height
+    x2 = x1 + width
+
+    im[y1:y1 + stroke, x1:x2] = rgb
+    im[y2:y2 + stroke, x1:x2] = rgb
+    im[y1:y2, x1:x1 + stroke] = rgb
+    im[y1:y2, x2:x2 + stroke] = rgb
+
+    return im
+	
+@app.route("/patient_plot")
+def display_bbox(x, y, width, height, im):
+    rgb = np.array([255,0,0]).astype('int')
+    box = [x,y,width,height]
+    im = overlay_box(im=im, box=box, rgb=rgb, stroke=6)
+    plt.imshow(im, cmap=pylab.cm.gist_gray)
+    #plt.show()
+    plt.imsave('./static/images/patient_plot.png', im)
 
 @app.route("/blank")
 def blank():
