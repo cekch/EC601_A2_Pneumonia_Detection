558cf5aabd459d9cfc3fade06559a84bbb8261a2
Eric <ericli21@bu.edu>
2018-12-12 11:16:51 -0500

    Updated iou metrics for submission csv

--- Mask RCNN/iou_per_image.py
+++ Mask RCNN/iou_per_image.py
@@ -12,8 +12,25 @@ def read_predicted(WORD_FILE):
         for line in ins:
             line = line[:-1]
             line = line.split(",")
+            list_bboxes = []
             if len(line) == 2:
-                print(line[1])
+                if line[1] != '' and line[1][0] == ' ':
+                    line[1] = line[1][1:]
+                value_split = line[1].split(" ")       
+                #print(value_split)
+                for index, value in enumerate(value_split):
+                    bbox = []
+                    if index % 5 == 0:
+                        bbox.extend(value_split[index:index+5])
+                    if bbox != []:
+                        if bbox != [''] and bbox != ['PredictionString']:
+                            list_bboxes.append(bbox)
+            if line[0] != 'patientId':
+                all_data[line[0]] = list_bboxes
+            #print(list_bboxes)
+            '''
+            if len(line) == 2:
+                #print(line[1])
                 value_split = line[1].split(" ")
                 
                 value_split = value_split[1:]
@@ -22,13 +39,16 @@ def read_predicted(WORD_FILE):
                 for index, value in enumerate(value_split):
                     bbox = []
                     if index % 5 == 0:
-                        bbox.extend(value_split[index+1:index+5])
+                        bbox.extend(value_split[index:index+5])
                     if bbox != []:
                         all_bboxes.append(bbox)
                     #print(all_bboxes)
             else:
                 all_bboxes = []
             all_data[line[0]] = all_bboxes
+            '''
+    #for key in all_data:
+    #    print(key, all_data[key])
     return all_data
 def read_real(WORD_FILE):
     all_data = collections.defaultdict(list)
@@ -43,47 +63,63 @@ def read_real(WORD_FILE):
 
 
 def iou():
-    print("Done")
-    predicted_data = read_predicted("submission_chexnet 2.csv")
-    real_data = read_real("stage_2_train_labels.csv")
+    #print("Done")
+    predicted_data = read_predicted("submission_resnet_new.csv")
+    #print(len(predicted_data))
+    real_data = read_real("stage_2_train_labels.csv")    
     all_iou = []
-    print("Done")
+    normal_iou = []
+    pneumonia_iou = []
+    #print("Done")
     for patient_id in predicted_data:
-        #print(patient_id)
-        image0 = np.zeros((1024,1024))
-        image1 = np.zeros((1024,1024))
+        predictions = np.zeros((1024,1024))
+        grounds = np.zeros((1024,1024))
         # print("Done")
-        # #predicted_bboxes = predicted_data['23ca0450-4138-4e7a-9489-0f5b6a91031d']
-        # #real_bboxes = real_data['23ca0450-4138-4e7a-9489-0f5b6a91031d']
+        #predicted_bboxes = predicted_data['0e533087-5959-46e5-9fa5-95761a7d472f']
+        #real_bboxes = real_data['0e533087-5959-46e5-9fa5-95761a7d472f']
         predicted_bboxes = predicted_data[patient_id]
         real_bboxes = real_data[patient_id]
-        #print(predicted_bboxes)
-        #print(real_bboxes)
         for bbox in predicted_bboxes:
             if bbox != []:
-                print(bbox)
-                x = int(float(bbox[0]))
-                y = int(float(bbox[1]))
-                w = int(float(bbox[2]))
-                h = int(float(bbox[3]))
-                image0[x:(x+w), y:(y+h)] = 1
+                x = int(float(bbox[1]))
+                y = int(float(bbox[2]))
+                w = int(float(bbox[3]))
+                h = int(float(bbox[4]))
+                predictions[x:(x+w), y:(y+h)] = 1
         for rbox in real_bboxes:
             if rbox[0] != '' and rbox[0] != 'x':
                 x2 = int(float(rbox[0]))
                 y2 = int(float(rbox[1]))
                 w2 = int(float(rbox[2]))
                 h2 = int(float(rbox[3]))
-                image1[x2:(x2+w2), y2:(y2+h2)] = 1
-        image2 = np.logical_and(image0,image1)
-        image3 = np.logical_or(image0,image1)
-        intersection_count = sum(sum(image2))
-        union_count = sum(sum(image3))
-        if union_count != 0:
+                grounds[x2:(x2+w2), y2:(y2+h2)] = 1
+        intersect = np.logical_and(predictions, grounds)
+        un = np.logical_or(predictions, grounds)
+        intersection_count = sum(sum(intersect))
+        union_count = sum(sum(un))
+        unhealth = sum(sum(grounds))
+        # plt.imshow(predictions)
+        # plt.figure()
+        # plt.imshow(grounds)
+        # plt.figure()
+        # plt.imshow(intersect)
+        # plt.figure()
+        # plt.imshow(un)
+        # plt.figure()
+        # plt.show()
+
+
+        if unhealth > 0:
+            pneumonia_iou.append(intersection_count/union_count)
             all_iou.append(intersection_count/union_count)
         else:
-            all_iou.append(1.0)
-        #plt.imshow(image2)
-        #plt.show()
+            if union_count > 0:
+                normal_iou.append(intersection_count/union_count)
+                all_iou.append(intersection_count/union_count)
+            else:
+                normal_iou.append(1.0)
+                all_iou.append(1.0)
+
         # image2 = image0 + image1
         # union_count = 0
         # intersection_count = 0
@@ -99,7 +135,17 @@ def iou():
         # else:
         #     image_iou = 1
         # all_iou.append(image_iou)
+    #print(pneumonia_iou)
+    print("Overall:")
     print(sum(all_iou) / float(len(all_iou)))
+    print("# of Normals:")
+    print(len(normal_iou))
+    print("Normal CXRs:")
+    print(sum(normal_iou) / float(len(normal_iou)))
+    print("# of Pneumonia:")
+    print(len(pneumonia_iou))
+    print("Pneumonia CXRs:")
+    print(sum(pneumonia_iou) / float(len(pneumonia_iou)))
 
 
     #for patient_id, bboxes in predicted_data.items():
