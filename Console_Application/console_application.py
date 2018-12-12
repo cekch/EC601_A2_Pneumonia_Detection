d386651be1c4d4fca007078969e85ebe2f5bb52d
Caroline <cekchian@bu.edu>
2018-12-12 01:24:42 -0500

    Fixed the errors that were occuring with the plotting for the ensemble model. Updated loss and accuracy information for resnet model in plot_loss.py.
    Added by Sarthak: Confidence levels are now being printed on the CXR
    plots along with the bounding boxes.

--- Console_Application/console_application.py
+++ Console_Application/console_application.py
@@ -23,6 +23,9 @@ import config
 from config import Config
 import utils
 from plot_loss import plot_stats
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 ROOT_DIR = '/project/ece601/A2_Pneumonia_Detection_MaskRCNN/'
 
@@ -30,17 +33,17 @@ class DetectorConfig(Config):
     """Configuration for training pneumonia detection on the RSNA pneumonia dataset.
     Overrides values in the base Config class.
     """
-    # Give the configuration a recognizable name  
+    # Give the configuration a recognizable name
     NAME = 'pneumonia'
     # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
     # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
     GPU_COUNT = 1
-    IMAGES_PER_GPU = 8 
-    
+    IMAGES_PER_GPU = 8
+
     BACKBONE = 'resnet50'
-    
+
     NUM_CLASSES = 2  # background + 1 pneumonia classes
-    
+
     IMAGE_MIN_DIM = 256
     IMAGE_MAX_DIM = 256
     #RPN_ANCHOR_SCALES = (32, 64, 128, 256)
@@ -51,7 +54,7 @@ class DetectorConfig(Config):
     DETECTION_NMS_THRESHOLD = 0.1
 
     STEPS_PER_EPOCH = 100
-    
+
 
 class InferenceConfig(DetectorConfig):
     GPU_COUNT = 1
@@ -71,6 +74,7 @@ def get_user_choice():
     print("[2] MaskRCNN")
     print("[3] ChexNet")
     print("[4] Plot Model Metrics")
+    print("[5] Ensemble")
     print("[q] Quit.")
 
     return input("Which model would you like to use? ")
@@ -91,11 +95,11 @@ def load_machine_learning_model(model_json, model_h5, model_type):
         inference_config = InferenceConfig()
         loaded_model = modellib.MaskRCNN(mode='inference', config=inference_config, model_dir=ROOT_DIR)
         loaded_model.load_weights(model_h5, by_name=True)
-            
+
     print("Loaded model from disk")
-        
+
     return loaded_model
-    
+
 
 def model_predict(model, test_image_info):
     filename = test_image_info[1][0]
@@ -164,7 +168,10 @@ def mask_rcnn_predict(model, image_fps, patient_id, min_conf=0.95):
                 bboxes_str = "{} {} {} {}".format(str(x1*resize_factor), str(y1*resize_factor), \
                                                    str(width*resize_factor), str(height*resize_factor))
                 out_str += bboxes_str
-    print("Prediction String = " + out_str)
+    if (out_str == ''):
+        print("No lung opacities found.")
+    else:
+        print("Mask-RCNN Prediction String = " + out_str)
     return out_str
 
 
@@ -232,7 +239,7 @@ def draw(pred_string, ground_truth, patient_id):
         ymin = pred_list[i+2]
         wid = pred_list[i+3]
         ht = pred_list[i+4]
-        box = [xmin,ymin,wid,ht]
+        box = [conf,xmin,ymin,wid,ht]
         predboxes.append(box)
         i=i+5
         num_boxes=num_boxes-1
@@ -251,7 +258,7 @@ def draw(pred_string, ground_truth, patient_id):
         j=j+4
         num_gt_boxes=num_gt_boxes-1
 
-    # --- Add boxes with random color if present
+    # --- Add boxes in red if present
     for box in predboxes:
         # rgb = np.floor(np.random.rand(3) * 256).astype('int')
         rgb = np.array([255,0,0]).astype('int')
@@ -260,7 +267,7 @@ def draw(pred_string, ground_truth, patient_id):
     for box in gtboxes:
         # rgb = np.floor(np.random.rand(3) * 256).astype('int')
         rgb = np.array([0,255,0]).astype('int')
-        im = overlay_box(im=im, box=box, rgb=rgb, stroke=6)
+        im = overlay_gtbox(im=im, box=box, rgb=rgb, stroke=6)
 
     plt.imshow(im, cmap=pylab.cm.gist_gray)
     # pyla.axis('off')
@@ -268,6 +275,38 @@ def draw(pred_string, ground_truth, patient_id):
     plt.show()
 
 def overlay_box(im, box, rgb, stroke=1):
+    """
+    Method to overlay single box on image
+
+    """
+    # --- Convert coordinates to integers
+    conf = box[0]
+    box = [int(float(b)) for b in box[1:]]
+
+    # --- Extract coordinates
+    y1, x1, height, width = box
+    y2 = y1 + height
+    x2 = x1 + width
+
+    im[y1:y1 + stroke, x1:x2] = rgb
+    im[y2:y2 + stroke, x1:x2] = rgb
+    im[y1:y2, x1:x1 + stroke] = rgb
+    im[y1:y2, x2:x2 + stroke] = rgb
+
+    pilimg = Image.fromarray(im)
+    draw = ImageDraw.Draw(pilimg)
+    font = ImageFont.truetype('Roboto-Bold.ttf', size=45)
+    (x, y) = ((x1+x2)/2, y1-46)
+    message = "conf: "+str(conf)[:5]
+    color = 'rgb(255, 255, 0)' # black color
+    draw.text((x, y), message, fill=color, font=font)
+    # im = np.array(draw)
+
+    im = np.array(pilimg)
+
+    return im
+
+def overlay_gtbox(im, box, rgb, stroke=1):
     """
     Method to overlay single box on image
 
@@ -287,6 +326,111 @@ def overlay_box(im, box, rgb, stroke=1):
 
     return im
 
+def draw_all(pred_string1, pred_string2, pred_string3, ground_truth, patient_id):
+    """
+    Method to draw single patient with bounding box(es) if present
+
+    """
+    # --- Open DICOM file
+    d = pydicom.read_file('/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/%s.dcm' % patient_id)
+    im = d.pixel_array
+
+    # --- Convert from single-channel grayscale to 3-channel RGB
+    im = np.stack([im] * 3, axis=2)
+
+    # --- Find Predicted boxes
+    pred_list1 = pred_string1.split()
+    pred_len1 = len(pred_list1)
+    num_boxes1 = pred_len1/5
+    i=0
+    predboxes1=[]
+    while(num_boxes1):
+        conf = pred_list1[i]
+        xmin = pred_list1[i+1]
+        ymin = pred_list1[i+2]
+        wid = pred_list1[i+3]
+        ht = pred_list1[i+4]
+        box = [conf, xmin,ymin,wid,ht]
+        predboxes1.append(box)
+        i=i+5
+        num_boxes1=num_boxes1-1
+
+    pred_list2 = pred_string2.split()
+    pred_len2 = len(pred_list2)
+    num_boxes2 = pred_len2/5
+    j=0
+    predboxes2=[]
+    while(num_boxes2):
+        conf = pred_list2[j]
+        xmin = pred_list2[j+1]
+        ymin = pred_list2[j+2]
+        wid = pred_list2[j+3]
+        ht = pred_list2[j+4]
+        box = [conf, xmin,ymin,wid,ht]
+        predboxes2.append(box)
+        j=j+5
+        num_boxes2=num_boxes2-1
+
+    pred_list3 = pred_string3.split()
+    pred_len3 = len(pred_list3)
+    num_boxes3 = pred_len3/5
+    k=0
+    predboxes3=[]
+    while(num_boxes3):
+        conf = pred_list3[k]
+        xmin = pred_list3[k+1]
+        ymin = pred_list3[k+2]
+        wid = pred_list3[k+3]
+        ht = pred_list3[k+4]
+        box = [conf, xmin,ymin,wid,ht]
+        predboxes3.append(box)
+        k=k+5
+        num_boxes3=num_boxes3-1
+
+    gt_len = len(ground_truth)
+    num_gt_boxes = gt_len/4
+    j=0
+    gtboxes=[]
+    while(num_gt_boxes):
+        xmin = ground_truth[j]
+        ymin = ground_truth[j+1]
+        wid = ground_truth[j+2]
+        ht = ground_truth[j+3]
+        box = [xmin,ymin,wid,ht]
+        gtboxes.append(box)
+        j=j+4
+        num_gt_boxes=num_gt_boxes-1
+
+    # --- Add boxes with random color if present
+    for box in predboxes1:
+        # rgb = np.floor(np.random.rand(3) * 256).astype('int')
+        rgb = np.array([255,255,0]).astype('int')
+        im = overlay_box(im=im, box=box, rgb=rgb, stroke=6)
+
+    for box in predboxes2:
+        # rgb = np.floor(np.random.rand(3) * 256).astype('int')
+        rgb = np.array([0,255,255]).astype('int')
+        im = overlay_box(im=im, box=box, rgb=rgb, stroke=6)
+
+    for box in predboxes3:
+        # rgb = np.floor(np.random.rand(3) * 256).astype('int')
+        rgb = np.array([255,0,255]).astype('int')
+        im = overlay_box(im=im, box=box, rgb=rgb, stroke=6)
+
+    for box in gtboxes:
+        # rgb = np.floor(np.random.rand(3) * 256).astype('int')
+        rgb = np.array([0,255,0]).astype('int')
+        im = overlay_gtbox(im=im, box=box, rgb=rgb, stroke=6)
+
+    plt.imshow(im, cmap=pylab.cm.gist_gray)
+    # pyla.axis('off')
+    # matplotlib.pyplot.show()
+    plt.show()
+
+#Load all models at start up
+resnet_model = load_machine_learning_model("./resnet_model.json", "./resnet_model.h5", '1')
+mask_rcnn_model = load_machine_learning_model("", "./mask_rcnn_model.h5", '2')
+chexnet_model = load_machine_learning_model("./chexnet_model.json", "./chexnet_model.h5", '3')
 
 
 choice = ''
@@ -296,37 +440,55 @@ while choice != 'q':
     # Respond to the user's choice.
     display_title_bar()
     if choice == '1':
-        # Load ResNet Model
-        resnet_model = load_machine_learning_model("./resnet_model.json", "./resnet_model.h5", choice)
+        #ResNet Model
         patient_id = get_patient_id()
         patient_test = find_patient_dcm_image(patient_id, "/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/")
         test_gen = Generator("/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/", patient_test, None, batch_size=20, image_size=256, shuffle=False, predict=True)
         pred_string = model_predict(resnet_model, test_gen[0])
         gt_bb = get_actual_bounding_box(patient_id)
         draw(pred_string,gt_bb,patient_id)
+        print("Red = ResNet Predictions")
+        print("Green = Ground Truth")
+        print
         pass
     elif choice == '2':
-        # Load MaskRCNN
-        mask_rcnn_model = load_machine_learning_model("", "./mask_rcnn_model.h5", choice)
+        #MaskRCNN Model
         patient_id = get_patient_id()
         patient_test = find_patient_dcm_image(patient_id, "/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/")
         #test_gen = Generator("/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/", patient_test, None, batch_size=8, image_size=256, shuffle=False, predict=True)
         pred_string = mask_rcnn_predict(mask_rcnn_model, "/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/" + patient_test[0], patient_id)
+        print("Red = Mask-RCNN Predictions")
+        print("Green = Ground Truth")
         gt_bb = get_actual_bounding_box(patient_id)
         draw(pred_string,gt_bb,patient_id)
         pass
     elif choice == '3':
-        # Load ChexNet
-        chexnet_model = load_machine_learning_model("./chexnet_model.json", "./chexnet_model.h5", choice)
+        #ChexNet Model
         patient_id = get_patient_id()
         patient_test = find_patient_dcm_image(patient_id, "/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/")
         test_gen = Generator("/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/", patient_test, None, batch_size=25, image_size=256, shuffle=False, predict=True)
         pred_string = model_predict(chexnet_model, test_gen[0])
         gt_bb = get_actual_bounding_box(patient_id)
+        print("Red = ChexNet Predictions")
+        print("Green = Ground Truth")
         draw(pred_string,gt_bb,patient_id)
         pass
     elif choice == '4':
         plot_stats()
+    elif choice == '5':
+        #Ensemble Model
+        patient_id = get_patient_id()
+        patient_test = find_patient_dcm_image(patient_id, "/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/")
+        test_gen = Generator("/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/", patient_test, None, batch_size=25, image_size=256, shuffle=False, predict=True)
+        pred_string1 = model_predict(resnet_model, test_gen[0])
+        pred_string2 = mask_rcnn_predict(mask_rcnn_model, "/project/ece601/A2_Pneumonia_Detection/Dataset/stage_1_test_images/" + patient_test[0], patient_id)
+        pred_string3 = model_predict(chexnet_model, test_gen[0])
+        gt_bb = get_actual_bounding_box(patient_id)
+        print("Yellow = ResNet Predictions")
+        print("Blue = Mask-RCNN Predictions")
+        print("Purple = ChexNet Predictions")
+        print("Green = Ground Truth")
+        draw_all(pred_string1, pred_string2, pred_string3, gt_bb, patient_id)
     elif choice == 'q':
         print("\nExiting.")
     else:
